df069e9e7d39a2304b8a3403c8a9b677e875ecf8
rifanid98 <adninsijawa@gmail.com>
2020-06-03 13:36:12 +0700

    -

--- introduction/datatypes.js
+++ introduction/datatypes.js
@@ -1,10 +1,15 @@
-
+/**
+ * Integer
+ */
 var age = 20;
 
 console.log(age); //20 yellow (int)
 // prototype bawaan integer
 console.log(age.toString() + '\n'); //20 white (string)
 
+/**
+ * String
+ */
 var name = 'John';
 console.log(name); //John
 // prototype bawaan string
@@ -16,7 +21,33 @@ console.log(name); //John
  */
 console.log(name.slice(0,2) + '\n'); //20 white (string)
 
-
+/**
+ * Function
+ */
 var sum = function(param1, param2) {
     console.log(param1+param2);
 }
+
+/**
+ * Object
+ */
+var profile = {
+    name: 'John',
+    age: 20
+}
+// update data object
+profile.name = 'Jojon';
+console.log(profile.name); // Jojon
+
+
+
+/**
+ * Array
+ */
+var fruits = ['buah1','buah2','buah3'];
+// contoh method dari array
+// menambah data atau element ke akhir array
+console.log(fruits.push('buah4')); // array fruits menjadi : ['buah1','buah2','buah3', 'buah4']
+// menghapus data atau element terakhir suatu array
+console.log(fruits.pop()); //['buah1', 'buah2']
+
